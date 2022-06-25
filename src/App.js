e4c3dc2dee2420e9133932fc94ebba22e2867071
khaled <khaledb.yahya@gmail.com>
2022-06-25 17:06:52 +0100

    improved logic & design

--- src/App.js
+++ src/App.js
@@ -5,41 +5,44 @@ function App() {
   const adminUser = {
     email: "admin@admin.com",
     password: "admin123",
-  }
+  };
 
-  const [user, setUser] = useState({name: "", email: ""});
+  const [user, setUser] = useState({ name: "", email: "" });
   const [error, setError] = useState("");
 
-  const Login = details => {
+  const Login = (details) => {
     console.log(details);
 
-    if (details.email === adminUser.email && details.password === adminUser.password){
+    if (
+      details.email === adminUser.email &&
+      details.password === adminUser.password
+    ) {
       console.log("Logged in!");
       setUser({
         name: details.name,
-        email: details.email
+        email: details.email,
       });
-    }
-    else {
+    } else {
       console.log("Details do not match!");
       setError("Details do not match!");
     }
-  }
+  };
 
   const Logout = () => {
-    setUser({name: "", email: ""});
-  }
-
+    setUser({ name: "", email: "" });
+  };
 
   return (
     <div className="App">
-      {(user.email !== "") ? (
+      {user.email !== "" ? (
         <div className="welcome">
-          <h2>Welcome, <span>{user.name}</span></h2>
+          <h2>
+            Welcome, <span className="userName">{user.name}</span>
+          </h2>
           <button onClick={Logout}>Logout</button>
         </div>
       ) : (
-        <LoginForm Login={Login} error={error}/>
+        <LoginForm Login={Login} error={error} />
       )}
     </div>
   );
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,36 +1,112 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
-function LoginForm({Login, error}) {
-    const [details, setDetails] = useState({name: "", email: "", password: ""});
+function LoginForm({ Login, error }) {
+  const [details, setDetails] = useState({
+    name: "",
+    email: "",
+    password: "",
+    gender: "",
+  });
 
-    const submitHandler = e => {
-        e.preventDefault();
+  console.log(details.gender);
 
-        Login(details);
-    }
+  const submitHandler = (e) => {
+    e.preventDefault();
 
+    Login(details);
+  };
+
+  function handleChange(event) {
+    setDetails((prevDetails) => {
+      return {
+        ...prevDetails,
+        [event.target.name]: event.target.value,
+      };
+    });
+  }
 
   return (
-    <form onSubmit={submitHandler}>
-        <div className='form-inner'>
-            <h2>Login</h2>
-            {(error !== "") ? ( <div className="error">{error}</div>) : ""}
-            <div className="form-group">
-                <label htmlFor="name">Name:</label>
-                <input type="text" name="name" id="name" onChange={e => setDetails({...details, name: e.target.value})} value={details.name}/>
-            </div>
-            <div className="form-group">
-                <label htmlFor="email">Email:</label>
-                <input type="email" name="email" id="email" onChange={e => setDetails({...details, email: e.target.value})} value={details.email}/>
-            </div>
-            <div className="form-group">
-                <label htmlFor="password"></label>
-                <input type="password" name="password" id="password" onChange={e => setDetails({...details, password: e.target.value})} value={details.password}/>
-            </div>
-            <input type="submit" value="LOGIN" />
+    <form onSubmit={submitHandler} autocomplete="off">
+      <div className="form-inner">
+        <h2>Sign In</h2>
+        {error !== "" ? <div className="error">{error}</div> : ""}
+        <div className="form-group">
+          <label htmlFor="name">Name:</label>
+          <input
+            autofocus
+            className="custom-input"
+            type="text"
+            name="name"
+            id="name"
+            onChange={handleChange}
+            value={details.name}
+            placeholder="Your Name"
+          />
+          <span class="underline"></span>
         </div>
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input
+            className="custom-input"
+            type="email"
+            name="email"
+            id="email"
+            onChange={handleChange}
+            value={details.email}
+            placeholder="Email"
+          />
+          <span class="underline"></span>
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input
+            className="custom-input"
+            type="password"
+            name="password"
+            id="password"
+            onChange={handleChange}
+            value={details.password}
+            placeholder="Create Password"
+          />
+          <span class="underline"></span>
+        </div>
+        <fieldset>
+          <legend>Gender</legend>
+
+          <input
+            type="radio"
+            id="male"
+            name="gender"
+            value="male"
+            onChange={handleChange}
+          />
+          <label htmlFor="male">Male</label>
+          <br />
+
+          <input
+            type="radio"
+            id="female"
+            name="gender"
+            value="female"
+            onChange={handleChange}
+          />
+          <label htmlFor="female">Female</label>
+          <br />
+
+          <input
+            type="radio"
+            id="other"
+            name="gender"
+            value="other"
+            onChange={handleChange}
+          />
+          <label htmlFor="other">Other</label>
+          <br />
+        </fieldset>
+        <input className="log-in" type="submit" value="LOGIN" />
+      </div>
     </form>
-  )
+  );
 }
 
-export default LoginForm
+export default LoginForm;
